519ba57303458c020b0a89384bcb2b2021093346
Radiant <bukoyeolaniyi@gmail.com>
2024-10-31 00:30:20 +0100

    create splash page

---
+++ app/(tabs)/profile.jsx
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Profile = () => {
+  return (
+    <View className='text-3xl min-h-screen w-full'>
+      <Text className='text-center items-center justify-center'>Profile Landing Page</Text>
+      {/* <textarea name="" id=""></textarea> */}
+    </View>
+  )
+}
+
+export default Profile
+
+const styles = StyleSheet.create({})
